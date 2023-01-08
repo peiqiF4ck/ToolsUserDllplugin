df02d50425788c5673d2ed8cd1b0b700be6c9a78
peiqiF4ck <jason-hkghost@qq.com>
2023-01-08 17:10:54 +0800

    Signed-off-by: peiqiF4ck

---
+++ Dllplugin/UserDll.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace Dllplugin
+{
+    class UserDll
+    {
+
+        /// <summary>
+        /// 只要返回不是 down error 程序将自动将结果写入文本中。如果想要自己实现 写文件那么需要返回结果加入|5a75574b96ce583b250cdcfff48caa78
+        /// 
+        /// </summary>
+        /// <param name="url"></param>
+        /// <returns></returns>
+        public static string DllExec(string url)
+        { //方法名称
+
+            string expResult = string.Empty;
+            string errorcase = "error";
+
+
+            string thinkphpRceRes = thinkphpRce(url);
+
+
+
+            if (errorcase.Equals(thinkphpRceRes))
+            {
+
+                expResult = errorcase;
+
+            }
+            else
+            {
+
+                expResult = thinkphpRceRes; //系统系统写结果
+
+                expResult = thinkphpRceRes + "|5a75574b96ce583b250cdcfff48caa78|"; //用户自己写成功结果系统不管 标志5a75574b96ce583b250cdcfff48caa78
+
+            }
+
+            return expResult;
+        }
+
+
+        /// <summary>
+        /// http请求
+        /// </summary>
+        /// <param name="url"></param>
+        /// <returns></returns>
+        private static string httpSendMethod(string url)
+        {
+            string result = string.Empty;
+
+
+
+            return result;
+        }
+
+        /// <summary>
+        /// http漏洞代码实现  
+        /// down 表示网站无法访问
+        /// error 表示没有漏洞 返回error
+        /// 成功则返回结果
+        /// </summary>
+        /// <param name="url"></param>
+        /// <returns></returns>
+        private  static string thinkphpRce(string url)
+        {
+            string result = string.Empty;
+
+            ///调用httpSendMethod 实战漏洞逻辑
+            ///
+            string errorcase = "error";
+
+            string res = httpSendMethod(url); //调用http请求请求结果
+
+
+            if (res.Contains("有漏洞标志")) //
+            {
+                //通过正则表达式方式直接刷选出漏洞然后赋值结果
+
+                string res1 = res.Split('|')[0]; //编程思维逻辑思维方式思考结果
+                result = res1;  //最终结果
+
+            }else
+            {
+
+
+                result = errorcase;
+            }
+
+
+
+            return result;
+        }
+
+
+    }
+}
---
+++ README.md
@@ -0,0 +1,14 @@
+# ToolsUserDllplugin 用户自定义模式自己编写利用dll代码
+工具配套用户自定义插件模板项目,自定义项目.用户自己编写dll.注意生成dll为:AnyCPU 64位优先 生成库为 4.5.1
+<h1>UserDllpluginExpGenerate Exp自动生成器。</h1>
+<ul>
+  <li>使用介绍</li>
+</ul>
+<h2> Exp自动生成器上图实测自动生成dll</h2>
+自动生成dll,并且加载利用。
+
+![exp生成器](https://raw.githubusercontent.com/peiqiF4ck/ToolsUserDllplugin/main/UserDll.png)<br/>
+
+实战利用
+
+![实战利用](https://raw.githubusercontent.com/peiqiF4ck/ToolsUserDllplugin/main/shizhan.png)<br/>
